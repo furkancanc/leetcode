6b9baaa79917f474616c53ab3939825febd21036
furkancanc <fcc.ceylan@gmail.com>
2025-01-11 21:22:55 +0300

    ConcatenationOfArray

---
+++ ConcatenationOfArray/concatenationOfArray.cpp
@@ -0,0 +1,42 @@
+// Source : https://leetcode.com/problems/concatenation-of-array
+// Date : 2024-01-11
+
+
+/*
+Given an integer array nums of length n, you want to create an array ans of length 2n where ans[i] == nums[i] and ans[i + n] == nums[i] for 0 <= i < n (0-indexed).
+
+Specifically, ans is the concatenation of two nums arrays.
+
+Return the array ans.
+
+ 
+
+Example 1:
+
+Input: nums = [1,2,1]
+Output: [1,2,1,1,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+- ans = [1,2,1,1,2,1]
+Example 2:
+
+Input: nums = [1,3,2,1]
+Output: [1,3,2,1,1,3,2,1]
+Explanation: The array ans is formed as follows:
+- ans = [nums[0],nums[1],nums[2],nums[3],nums[0],nums[1],nums[2],nums[3]]
+- ans = [1,3,2,1,1,3,2,1]
+ 
+*/
+
+#include <vector>
+using namespace std;
+
+class Solution 
+{
+public:
+    vector<int> getConcatenation(vector<int>& nums) 
+    {
+        nums.insert(nums.end(), nums.begin(), nums.end());
+        return nums;
+    }
+};
