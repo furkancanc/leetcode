3e9bbac57593ce2ad2517278347d73ad5e596b68
furkancanc <fcc.ceylan@gmail.com>
2025-01-18 21:26:19 +0300

    InsertGreatestCommonDivisorsInLinkedList

---
+++ InsertGreatestCommonDivisorsInLinkedList/insertGreatestCommonDivisorsInLinkedList.cpp
@@ -0,0 +1,81 @@
+// Source : https://leetcode.com/problems/unique-morse-code-words
+// Date : 2024-01-18
+
+/*
+Given the head of a linked list head, in which each node contains an integer value.
+
+Between every pair of adjacent nodes, insert a new node with a value equal to the greatest common divisor of them.
+
+Return the linked list after insertion.
+
+The greatest common divisor of two numbers is the largest positive integer that evenly divides both numbers.
+
+ 
+
+Example 1:
+
+
+Input: head = [18,6,10,3]
+Output: [18,6,6,2,10,1,3]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes (nodes in blue are the inserted nodes).
+- We insert the greatest common divisor of 18 and 6 = 6 between the 1st and the 2nd nodes.
+- We insert the greatest common divisor of 6 and 10 = 2 between the 2nd and the 3rd nodes.
+- We insert the greatest common divisor of 10 and 3 = 1 between the 3rd and the 4th nodes.
+There are no more adjacent nodes, so we return the linked list.
+Example 2:
+
+
+Input: head = [7]
+Output: [7]
+Explanation: The 1st diagram denotes the initial linked list and the 2nd diagram denotes the linked list after inserting the new nodes.
+There are no pairs of adjacent nodes, so we return the initial linked list.
+*/
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+class Solution 
+{
+public:
+    ListNode* insertGreatestCommonDivisors(ListNode* head) 
+    {
+        if (head == nullptr || head->next == nullptr) return head;
+
+        ListNode* node_1 = head;
+        ListNode* node_2 = head->next;
+
+        while (node_2 != nullptr)
+        {
+            int gcd = calculateGCD(node_1->val, node_2->val);
+
+            ListNode* gcdNode = new ListNode(gcd);
+            node_1->next = gcdNode;
+            gcdNode->next = node_2;
+
+            node_1 = node_2;
+            node_2 = node_2->next;
+        }
+
+        return head;
+    }
+
+    int calculateGCD(int a, int b)
+    {
+        while (b != 0)
+        {
+            int temp = b;
+            b = a % b;
+            a = temp;
+        }
+
+        return a;
+    }
+};
