13fe088605b673a3139da4f719c7982f47f54ca1
furkancanc <fcc.ceylan@gmail.com>
2025-03-10 23:15:33 +0300

    SearchInABinaryTree

---
+++ SearchInABinaryTree/searchInABinaryTree.cpp
@@ -0,0 +1,51 @@
+// Source : https://leetcode.com/problems/search-in-a-binary-search-tree
+// Date : 2024-03-10
+
+/*
+You are given the root of a binary search tree (BST) and an integer val.
+
+Find the node in the BST that the node's value equals val and return the subtree rooted with that node. If such a node does not exist, return null.
+
+ 
+
+Example 1:
+
+
+Input: root = [4,2,7,1,3], val = 2
+Output: [2,1,3]
+Example 2:
+
+
+Input: root = [4,2,7,1,3], val = 5
+Output: []
+ 
+*/
+
+
+ // Definition for a binary tree node.
+ struct TreeNode {
+     int val;
+     TreeNode *left;
+     TreeNode *right;
+     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ };
+ 
+
+class Solution 
+{
+    public:
+        TreeNode* searchBST(TreeNode* root, int val) 
+        {
+            if (!root) 
+                return nullptr;
+
+            if (root->val == val)
+                return root;
+            else if (root->val > val)
+                return searchBST(root->left, val);
+            else
+                return searchBST(root->right, val);
+        }
+};
